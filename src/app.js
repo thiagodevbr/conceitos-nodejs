de19d167c61d83f8d8539dca70f74a6dc0294690
Thiago <thiago.brito@aluno.faculdadeimpacta.com.br>
2020-04-11 21:33:02 -0300

    Updated project finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,59 @@ app.use(cors());
 
 const repositories = [];
 
+const verifyRepository = (req, res, next) => {
+  const { id } = req.params;
+  const index = repositories.findIndex((repo) => repo.id === id);
+  if (index >= 0) return next();
+  else return res.status(400).json({ error: "Repository does not exists" });
+};
+
+app.use("/repositories/:id", verifyRepository);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const data = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  repositories.push(data);
+  return response.json(data);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const index = repositories.findIndex((repo) => repo.id === id);
+  const { likes } = repositories[index];
+  repositories[index] = {
+    id: id,
+    title,
+    url,
+    techs,
+    likes,
+  };
+  return response.json(repositories[index]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex((repo) => repo.id === id);
+  repositories.splice(index, 1);
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex((repo) => repo.id === id);
+  repositories[index].likes++;
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
